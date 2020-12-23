04524ff879f7a0ff3cbdb88b79bcc5bd4d017dd0
Satish kumar pradhan <63499686+satish-313@users.noreply.github.com>
2020-12-23 23:44:50 +0530

    added next in verify token

--- node-auth-jwt/verifyToken.js
+++ node-auth-jwt/verifyToken.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 module.exports = function (req,res,next) {
   const token = req.header('auth-token');
   if(!token) return res.status(404).send('Access denied')
-
+  next()
   try {
     const verified = jwt.verify(token,process.env.TOKEN_SECRET);
     req.username = verified;
